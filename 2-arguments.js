2a97ba774b38c5e7255b8bf1ca75347deb6d8b3b
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-05-21 23:12:56 +0100

    update

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,6 +2,8 @@ const myVar = process.argv.slice(2);
 
 if (myVar.length === 0) {
   console.log('No argument');
+} else if (myVar.length === 1) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
