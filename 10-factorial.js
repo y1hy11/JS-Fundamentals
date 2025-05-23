ff420d1110b3859568f1a748a0baa7339061dd07
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-05-23 17:02:07 +0100

    update

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+const factorial = (n) => {
+    const num = parseInt(n);
+    
+    if (isNaN(num)) return 1;    
+    if (num <= 0) return 1;
+    if (num === 1) return 1;
+    
+    return num * factorial(num - 1);
+};
+
+const number = process.argv[2];
+console.log(factorial(number));
